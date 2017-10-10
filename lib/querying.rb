95fa42f162752ff609ceba9d0be55449e023fe87
Justin Daniel <justinddaniel@gmail.com>
2017-10-10 19:14:02 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters WHERE length(motto) = (SELECT MAX(LENGTH(motto)) FROM characters);"
+  "SELECT characters.name, characters.motto FROM characters WHERE length(motto) = (SELECT MAX(length(motto)) FROM characters);"
 end
 
 
