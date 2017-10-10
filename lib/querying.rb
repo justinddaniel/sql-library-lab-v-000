25746f53a571547b586197d78e8674d3f9024317
Justin Daniel <justinddaniel@gmail.com>
2017-10-10 19:26:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,8 +9,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) AS number FROM characters
-  GROUP BY number;"
+  "SELECT characters.species FROM characters
+  GROUP BY characters.species;"
 end
 
 def select_name_and_series_subgenres_of_authors
