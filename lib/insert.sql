9c1622b0fa5f08fe51ae81495292ec7474f6a472
Justin Daniel <justinddaniel@gmail.com>
2017-10-10 18:36:15 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES 
+INSERT INTO series (title, author_id, subgenre_id) VALUES
 ("WASSUP", 1, 1), ("doh", 2, 2);
 
 INSERT INTO books (title, year, series_id) VALUES
@@ -7,4 +7,14 @@ INSERT INTO books (title, year, series_id) VALUES
 ("HER", 1993, 3),
 ("FER", 1994, 4),
 ("SER", 1999, 5),
-("TER", 1989, 6)
+("TER", 1989, 6);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES 
+("GOOFY", "BE GOOFY", "HUMAN", 1, 2),
+("SPOOFY", "BE SPOOFY", "HUMAN", 1, 2),
+("TOOFY", "BE TOOFY", "HUMAN", 1, 2),
+("LOOFY", "BE LOOFY", "HUMAN", 2, 1),
+("NOOFY", "BE NOOFY", "DINOSAUR", 2, 1),
+("POOFY", "BE POOFY", "DINOSAUR", 2, 1),
+("BOOFY", "BE BOOFY", "DINOSAUR", 2, 1),
+("FOOFY", "BE FOOFY", "DINOSAUR", 2, 1);
