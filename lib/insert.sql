0daeebe7c2dd36c77143f4e2caf2840cb235d80f
Justin Daniel <justinddaniel@gmail.com>
2017-10-10 18:46:13 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -23,4 +23,6 @@ INSERT INTO subgenres (name) VALUES ("comedy"), ("dinosaurology");
 
 INSERT INTO authors (name) VALUES ("GENGHIS KHAN"), ("BOZO THE CLOWN");
 
-
+INSERT INTO character_books (book_id, character_id) VALUES
+(1, 1), (1, 1), (1, 2), (2, 2), (2, 3), (2, 3), (3, 4), (3, 4), 
+(4, 5), (4, 5), (5, 6), (5, 6), (6, 7), (6, 7), (6, 8), (6, 8);
