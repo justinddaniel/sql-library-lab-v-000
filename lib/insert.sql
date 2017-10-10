6860502cdf699fc5927b18209f6cd838d343eb8a
Justin Daniel <justinddaniel@gmail.com>
2017-10-10 18:31:42 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES 
+("WASSUP", 1, 1), ("doh", 2, 2);
+
+INSERT INTO books (title, year, series_id) VALUES
+("DER", 1990, 1),
+("GER", 1992, 2),
+("HER", 1993, 3),
+("FER", 1994, 4),
+("SER", 1999, 5),
+("TER", 1989, 6)
